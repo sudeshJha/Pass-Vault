1a635b0079bcfe69326ea3e59c2ab6083065a1bf
Sudesh Jha <sudeshjha2005@gmail.com>
2026-09-07 22:03:10 +0530

    UI design

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 from tkinter import *
 
+FONT = ("Arial", 12, "bold")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
-window.title("Pass Vault App")
-window.config(padx=20, pady=20)
+window.title("Password Manager")
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=40)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=40)
+password_entry.grid(row=3, column=1, columnspan=2)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", width=20)
+generate_password_button.grid(row=4, column=1, columnspan=2)
+
+add_button = Button(text="Add", width=50)
+add_button.grid(row=5, column=0, columnspan=3)
+
+window.mainloop()
