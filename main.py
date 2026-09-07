d5f1c62782b86a6935340340c2dd4aef7d4f2e46
Sudesh Jha <sudeshjha2005@gmail.com>
2026-09-07 23:13:12 +0530

    save password

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tkinter import *
 
 FONT = ("Arial", 12, "bold")
+FILE_NAME = "data.txt"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open(file="data.txt", mode="a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,8 +42,12 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=40)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
 email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "sudesh@gmail.com")
+
 password_entry = Entry(width=40)
 password_entry.grid(row=3, column=1, columnspan=2)
 
@@ -38,7 +55,7 @@ password_entry.grid(row=3, column=1, columnspan=2)
 generate_password_button = Button(text="Generate Password", width=20)
 generate_password_button.grid(row=4, column=1, columnspan=2)
 
-add_button = Button(text="Add", width=50)
+add_button = Button(text="Add", width=50, command=save)
 add_button.grid(row=5, column=0, columnspan=3)
 
 window.mainloop()
